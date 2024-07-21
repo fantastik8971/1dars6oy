9bdcac585958e1523c2dc834371dc49a72c529e0
fantastik8971 <161023430+fantastik8971@users.noreply.github.com>
2024-07-21 13:21:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// setTimeout(function(){
+//     console.log("salom");
+// },10000)
+
+// setTimeout(function(){
+//     const newText = document.createElement("h1");
+//     newText.textContent = "salom"
+//     document.body.appendChild(newText)
+
+// },1000)
+
+
+// let count = 0;
+// const newText = document.createElement("h1")
+// setInterval(function(){
+// newText.textContent = ++count;
+// document.body.appendChild(newText)
+// if (count === 13){
+
+//     const happyBirhtday = textContent = document.createElement("h2")
+//     happyBirhtday.textContent =`${count} yosh muborak`
+//     document.body.appendChild(happyBirhtday)
+//     newText.style.display = "none"
+//     clearInterval(time)
+// }
+// },1000)  
+
